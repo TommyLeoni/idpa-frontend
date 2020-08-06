77c91afe7e947bcdd1d3fceb8c826ae5a1f1b5fe
TommyLeoni <tomasoleoni011@gmail.com>
2020-08-06 16:56:42 +0200

    error catch for danger

--- src/Results_new/ResultComponent.js
+++ src/Results_new/ResultComponent.js
@@ -24,35 +24,37 @@ export default function ResultComponent(danger, sentences, key) {
   } catch (err) { }
 
   return (
-    <div>
-      {danger ?
-        <div className={`row p-3`}>
-          <div className="col-3 col-md-2 res-bg text-light py-4 my-2 rounded-corners-left shadow">
-            <div className="my-auto">
-              <h1 className="font-weight-bold">
-                <u className="id">{key}</u>
-              </h1>
+    <div className="container" id="container">
+      <div className="row justify-content-center text-center h-100 m-0 p-0">
+        {danger ?
+          <div className={`row p-3`}>
+            <div className="col-3 col-md-2 res-bg text-light py-4 my-2 rounded-corners-left shadow">
+              <div className="my-auto">
+                <h1 className="font-weight-bold">
+                  <u className="id">{key}</u>
+                </h1>
+              </div>
+            </div>
+            <div className="col-9 col-md-10 bg-light py-4 my-2 rounded-corners-right text-left shadow">
+              <p className="my-auto">{danger.text}</p>
             </div>
           </div>
-          <div className="col-9 col-md-10 bg-light py-4 my-2 rounded-corners-right text-left shadow">
-            <p className="my-auto">{danger.text}</p>
-          </div>
-        </div>
 
-        : <></>}
-      {sentences.length > 0 ?
-        <>
-          <div className="w-100 text-center my-2 py-2 text-muted">
-            <u onClick={() => toggleView(!viewRest)}>{viewRest ? "Hide context" : "Show context"}</u>
-          </div>
-          <div className={`${viewRest ? "" : "d-none"} w-100 text-center my-2 py-2 text-muted`}>
-            {sentences.map(function (sentence, i) {
-              return <p key={i}>{sentence}</p>;
-            })}
-          </div>
-        </>
-        : <></>
-      }
+          : <></>}
+        {sentences.length > 0 ?
+          <>
+            <div className="w-100 text-center my-2 py-2 text-muted">
+              <u onClick={() => toggleView(!viewRest)}>{viewRest ? "Hide context" : "Show context"}</u>
+            </div>
+            <div className={`${viewRest ? "" : "d-none"} w-100 text-center my-2 py-2 text-muted`}>
+              {sentences.map(function (sentence, i) {
+                return <p key={i}>{sentence}</p>;
+              })}
+            </div>
+          </>
+          : <></>
+        }
+      </div>
     </div>
   );
 }
